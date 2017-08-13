883713aebda356bd1e356c9296155c471ac165e9
fabrizio-stocco <fabrizio.stocco@fabriziostocco.xyz>
2017-08-13 20:13:45 +0200

    Initial Commit

---
+++ DocumentBased/Document.swift
@@ -0,0 +1,22 @@
+//
+//  Document.swift
+//  DocumentBased
+//
+//  Created by Fabrizio Stocco on 13/08/17.
+//  Copyright © 2017 Fabrizio Stocco. All rights reserved.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
